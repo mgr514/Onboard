c0c773f008e5994d6fde2b4e25f5e0b91708cac0
mgr514 <mgr514@mun.ca>
2024-02-01 11:30:43 -0330

    small edits to existing code

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   // ============================ CONST =======================================
-  const [formTitle, setFormTitle] = useState('');   // State to store the form title
-  const [selectedItem, setSelectedItem] = useState(''); //State to store selected list item
+  const [formTitle, setFormTitle] = useState('localStorage.getItem'); 
+  const [selectedItem, setSelectedItem] = useState(''); 
 
 
   //========================== FUNCTIONS =============================================
-  //load form title from local storage -> see notes below for useEffect if need refresher
   //this is a hook, is that different from a typical function or is a hook a type of function? confused on language.
   useEffect(() => {
-    const storedTitle = localStorage.getItem('formTitle');
+    const storedTitle = ('formTitle');
     if (storedTitle) {
       setFormTitle(storedTitle);
     }
   }, []);
 
-  //Function that handles form title change and saves to local storage
+
   const handleFormTitleChange = (e) => {
     const newTitle = e.target.value;
     setFormTitle(newTitle)
@@ -25,9 +24,8 @@ function App() {
   }
 
 
-//SELECTION HANDLER
-const handleSelectChange= (event) => {
-  setSelectedItem(event.target.value);
+const handleSelectChange= (e) => {
+  setSelectedItem(e.target.value);
 };
 
 
@@ -69,22 +67,3 @@ const handleSelectChange= (event) => {
 }
 
 export default App
-
-
-//=====================NOTES===============================
-
-// const handleAddDropdown = () => {
-//   setSelectedItems([...selectedItems, '']); 
-
-//setSelectedItems is function provided by useState hook to update state variable selectedItems
-//...selectedItems creates new array with elements of existing array and adds empty string '' to the end
-
-//USE EFFECT
-//hook called after component is rendered
-//[] is a dependency that means it runs once after the initial render
-//useEffecr allows you to perform side effects in functional components, side effects
-//include dtata fetching, manual DOM manipulations, subscriptions, etc
-//it takes 2 arguments: function containing code for side effect, and optional array of dependencies
-//in context of local storage:
-//1 loading data on mount (initialize state, load from API, retriece from local storage)
-//2 persisting data, when a user interacts with component and changes data, data may sruvive page relaods
