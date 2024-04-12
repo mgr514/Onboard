ab05dea784b756040e3a12519fd2251e6b753975
mgr514 <mgr514@mun.ca>
2024-04-12 13:46:40 -0230

    info added to home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Booklet.jsx";
+import Booklet from "./Booklet.jsx";
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./routes/Home";
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,9 +5,16 @@ function Home() {
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>
-        This project is a patient education tool to be used when onboarding a
-        patient for surgery or procedures. Editable fields allow the educator to
-        change and update any information for individualized patient education.
+        I currently work as a nurse in a hospital. Patients requiring education
+        about procedures, surgeries, and/or recovery are provided paper
+        booklets. Unfortunately the standardized booklet is not accessible for
+        all patients. Patients have increasing complex needs that a booklet
+        cannot address, as well some patients are illiterate, blind, or
+        non-english speaking. This project is a patient education tool to be
+        used when onboarding a patientwith their health plan is needed. Editable
+        fields allow the educator to change and update any information for
+        individualized patient education, allowing us to easily address
+        individual and complex needs.
       </p>
     </div>
   );
