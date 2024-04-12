76306cb4655decd07de452b7c592c8eeb5404596
mgr514 <mgr514@mun.ca>
2024-04-12 13:37:20 -0230

    file organizationx2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./routes/Booklet.jsx";
+import App from "./Booklet.jsx";
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./routes/Home";
 
