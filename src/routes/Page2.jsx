178d798d72c1c5b08fbecb6830c312dcb777e9c8
mgr514 <mgr514@mun.ca>
2024-04-12 22:45:34 -0230

    page 2 edits

--- src/routes/Page2.jsx
+++ src/routes/Page2.jsx
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from "react";
 
 function Page2() {
-  const [data, setData] = useState();
+  const [formTitle, setFormTitle] = useState("");
+  const [points, setPoints] = useState([]);
 
   useEffect(() => {
-    const data = localStorage.getItem("bookletData");
-    if (data) {
-      setData(JSON.parse(data));
+    const storedData = localStorage.getItem("bookletData");
+    if (storedData) {
+      const data = JSON.parse(storedData);
+      setFormTitle(data.formTitle);
+      setPoints(data.points || []);
     }
   }, []);
 
-  if (!data) {
+  if (!formTitle && points.length === 0) {
     return <div>Loading data...</div>;
   }
 
   return (
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-bold text-center text-gray-900 my-5">
-        {title}
+        {formTitle}
       </h1>
       <ul className="list-disc space-y-2 p-5">
         {points.map((point, index) => (
           <li key={index} className="text-gray-700 text-base">
-            {point}
+            {points}
           </li>
         ))}
       </ul>
