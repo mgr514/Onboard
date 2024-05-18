9dceb27b888a44c548ba3c35786c7698a7703d20
mgr514 <mgr514@mun.ca>
2024-05-17 22:40:28 -0230

    home edits

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,58 +4,73 @@ import FontSizeToggle from "../components/Fontaccess";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-black text-white">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+      <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8">
         <FontSizeToggle />
-        <h1 className="text-3xl font-bold text-center text-gray-900 mt-10 mb-8 dark:text-white">
+        <h1 className="text-4xl font-extrabold text-center mt-10 mb-10 dark:text-white">
           Welcome to MedonBoard
         </h1>
-        <div className="flex justify-center mb-8">
+        <div className="flex justify-center mb-12">
           <Link
             to="/library"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-lg transition-colors duration-300"
           >
             Go to Library
           </Link>
         </div>
-        <div className="space-y-6">
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-            About Me:
-          </h2>
-          <p className="text-base text-gray-700 dark:text-white">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl font-bold mb-6">About Me:</h2>
+          <p className="text-lg mb-6">
             Hi! My name is Michaela and I'm from St. John's, Newfoundland. I am
             a Get Coding student, but I also have an educational background in
             nursing and business administration. I currently work as a nurse in
-            a CVICU caring for patient post heart surgery.
+            a CVICU caring for patients post heart surgery.
           </p>
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-            What is this app?
-          </h2>
-          <p className="text-base text-gray-700 dark:text-white">
-            This is a Get Coding project that is meant to be a patient education
-            tool. It onboards patients for any medical
-            procedure/surgery/recovery they may experience.
-          </p>
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-            Issue:
-          </h2>
-          <p className="text-base text-gray-700 dark:text-white">
-            One of my pain points in my role as a nurse is that patient
-            education is not adequate. We currently provide patients paper
-            booklets in English and that's the only resource we provide. The
-            issue is that many patients do not have the literacy level to
-            understand or even read the booklets we provide, as well as patients
-            who have poor vision.
-          </p>
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-            Solution:
-          </h2>
-          <p className="text-base text-gray-700 dark:text-white">
-            Using Tailwind and React, I have created an app that allows an
-            educator/staff member to create booklets using multiple media types.
-            These booklets will store in local storage for recall and editing.
-            The multimedia options allow an educator to maximize patient
-            education by creating resources that can benefit all learning needs.
+          <img
+            src="URL_TO_YOUR_IMAGE"
+            alt="IMAGE HERE"
+            className="mx-auto rounded-full shadow-lg w-32 h-32 object-cover"
+          />
+        </div>
+        <div className="md:grid md:grid-cols-2 md:gap-12 mb-12">
+          <div>
+            <h2 className="text-2xl font-semibold text-center mb-4">
+              What is this app?
+            </h2>
+            <p className="text-md">
+              This Get Coding project is designed as a patient education tool,
+              facilitating the onboarding of patients for various medical
+              procedures.
+            </p>
+            <img
+              src="URL"
+              alt="IMAGE HERE"
+              className="mt-4 w-full rounded-lg shadow-md"
+            />
+          </div>
+          <div className="mt-12 md:mt-0">
+            <h2 className="text-2xl font-semibold text-center mb-4">
+              Why this app?
+            </h2>
+            <p className="text-md">
+              One of my pain points is in how we provide patient education.
+              Addressing a major gap in patient education, this app provides
+              customizable booklets to aid understanding across literacy levels
+              and other accessibility barriers.
+            </p>
+            <img
+              src="URL"
+              alt="IMAGE HERE"
+              className="mt-4 w-full rounded-lg shadow-md"
+            />
+          </div>
+        </div>
+        <div className="text-center mb-10">
+          <h2 className="text-3xl font-bold mb-6">Solution:</h2>
+          <p className="text-lg">
+            Using React and Tailwind, this app enables educators to create
+            versatile, media-rich educational booklets stored in local storage
+            for easy editing and reuse.
           </p>
         </div>
       </div>
